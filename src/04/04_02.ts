c5a6c3f34c8b38467e673a124a2f76c0ed10acf1
Eugen <vasilenkoevgen88@gmail.com>
2023-07-09 19:19:30 +0300

    First commit

--- src/04/04_02.ts
+++
@@ -1,5 +0,0 @@
-import {CityType} from '../02/02_02';
-
-export function demolishHousesOnTheStreet(city: CityType, street: string){
-    city.houses = city.houses.filter(house=> house.address.street.title !== street)
-}
